09d1ff1236dd2284156926285a461f05b1e3a497
Anowarforhad <45456586+Anowarforhad@users.noreply.github.com>
2018-12-13 15:53:50 +0600

    java

---
+++ 1st-javascript.js
@@ -0,0 +1,55 @@
+
+
+
+
+var food = [' tea', 'coffee', 'milk', 'barger'];
+console.log(food);
+var roll = [9,4,5,6,7,7,34,2,4,5]
+console.log(roll[2]);
+
+
+
+
+
+var fname  = "anowar";
+var lname = "forhad";
+var fullname = fname + lname;
+console.log (fullname);
+
+
+
+
+var a;
+a = 5;
+var b = 5;
+var c = 3;
+var c = b +c;
+console.log(c);
+console.log(a);
+
+
+
+var food  = ['barger', 'pizza', 'cake','tea', 'coffee','milk'];
+ var price = [ 100,110, 50, 20, 20,50];
+ var barger = 0;
+ var pizza = 1;
+ var cake = 2;
+  var  tea = 3;
+  var coffee = 4;
+  var milk = 5;
+  console.log(price[milk]);
+
+
+
+  var i = 0;
+  loop_spring = '';
+  while (i<=10){
+  	
+  	loop_spring = loop_spring + '-';
+  	console.log(loop_spring);
+  	i++;
+  }
+
+
+
+
